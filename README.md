9d8c347014451ea36e4e4af1daa2723cba185dcb
Gilga <gilga0036@gmail.com>
2024-03-09 18:13:39 +0700

    code by:gilga

---
+++ README.md
@@ -0,0 +1,62 @@
+### GAMON SIMPLE AI BOT DISCORD.JS
+code by: gilga
+using API gemini ai [https://aistudio.google.com/app/apikey](https://aistudio.google.com/app/apikey)
+npm list 
+├── @discordjs/rest@0.4.1
+├── @google-ai/generativelanguage@2.1.0
+├── @google/generative-ai@0.2.1
+├── ascii-table@0.0.9
+├── chalk@2.4.2
+├── discord-api-types@0.30.0
+├── discord.js@14.12.1
+├── dotenv@16.4.5
+├── express@4.18.3
+├── google-auth-library@9.6.3
+├── ms@2.1.3
+└── node@16.14.0
+
+### Installation
+
+* change
+```sh
+.env.example
+```
+to
+```sh
+.env
+```
+# contents of the .env file
+* GET TOKEN [DEVELOVER PORTAL](https://discord.com/developers)
+```sh
+TOKEN=
+```
+* OPTIONAL
+```sh
+GUILD_ID=
+```
+* GET CLIENT ID [DEVELOVER PORTAL](https://discord.com/developers)
+```sh
+CLIENT_ID=
+```
+* GET API [Google Ai Studio](https://aistudio.google.com/app/apikey)
+```sh
+GEMINI_API=
+```
+# INSTALL & RUN
+* npm
+  ```sh
+  npm i
+  ```
+  # or
+  ```sh
+  npm install
+  ```
+
+* End
+  ```sh
+  node .
+  ```
+  or
+  ```sh
+  node server.js
+  ```
---
+++ server.js
@@ -0,0 +1,107 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var view = __dirname + "/views/";
+var public = __dirname + "/public/";
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(view + "index.html"));
+});
+
+app.use('/', express.static(public));
+app.listen(8080);
+
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds, 
+		GatewayIntentBits.GuildMessages, 
+		GatewayIntentBits.GuildPresences, 
+		GatewayIntentBits.GuildMessageReactions, 
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.MessageContent,
+	], 
+	partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction]
+});
+
+const fs = require('fs');
+const config = require('./config.json');
+require('dotenv').config()
+
+/* ------------------ COLLECTIONS ------------------ */
+client.commands = new Collection()
+client.aliases = new Collection()
+client.events = new Collection();
+client.slashCommands = new Collection();
+client.prefix = config.prefix
+
+module.exports = client;
+
+const MODEL_NAME = "gemini-pro";
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
+
+client.on("messageCreate", async (message) => {
+	if (message.author.bot) return;
+  
+	if (message.mentions.has(client.user)) {
+	  const userMessage = message.content
+		.replace(`<@!${client.user.id}>`, "")
+		.trim();
+	  console.log(`Pesan pengguna: ${userMessage}`);
+	  if (userMessage === userMessage) {
+		message.reply("> ### ↺ load message \n> " + userMessage)
+	  }
+	  try {
+		// Attempt to generate response using Google Generative AI
+  
+		const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+  
+		const generationConfig = {
+		  temperature: 0.9,
+		  topK: 1,
+		  topP: 1,
+		  maxOutputTokens: 2048,
+		};
+  
+		const parts = [
+		  {
+			text: `input: ${userMessage}`,
+		  },
+		];
+  
+		const result = await model.generateContent({
+		  contents: [{ role: "user", parts }],
+		  generationConfig,
+		});
+  
+		const reply = await result.response.text();
+		console.log(`Bot replied: ${reply}`);
+  
+		if (reply.length > 2000) {
+		  const replyArray = reply.match(/[\s\S]{1,2000}/g);
+		  replyArray.forEach(async (msg) => {
+			await message.reply(msg);
+		  });
+		  return;
+		}
+  
+		message.reply(reply);
+		if (reply === "") {
+		  message.reply("Maaf, saya mengalami masalah saat memproses pesan Anda.")
+		};
+	  } catch (error) {
+		console.error("Error generating response:", error); // Log the error
+		message.reply("bot error please report it to the developer\n[❒ EDTLY](https://discord.gg/uRDcjESqeB)"); // Inform user about the error
+	  }
+	}
+  });
+
+
+fs.readdirSync('./handlers').forEach((handler) => {
+	require(`./handlers/${handler}`)(client)
+});
+
+
+client.login(process.env.TOKEN)
